33a586feefac799edc0ae50215c0a7239877fd41
nuheajiohc <jaehun341012@gmail.com>
2023-02-17 15:33:25 +0900

    faet: 메뉴 수정하기 기능

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 // step1 요구사항 구현을 위한 전략
 // TODO 메뉴 추가
 // - [x] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
-// = [ ] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
+// - [x] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
 // - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // - [x] 추가되는 메뉴의 아래 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
 // - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
@@ -10,6 +10,21 @@
 const $ = selector => document.querySelector(selector);
 
 function App() {
+  // TODO 메뉴 수정
+  // - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 prompt 창이 뜬다.
+  // - [x] 모달창에서 신규 메뉴명을 입력 받고, 확인 버튼을 누르면 메뉴가 수정된다.
+
+  $("#espresso-menu-list").addEventListener("click", e => {
+    if (e.target.classList.contains("menu-edit-button")) {
+      const $menuName = e.target.closest("li").querySelector(".menu-name");
+      const updatedMenuName = prompt(
+        "메뉴명을 수정하세요",
+        $menuName.innerText,
+      );
+      $menuName.innerText = updatedMenuName;
+    }
+  });
+
   $("#espresso-menu-form").addEventListener("submit", e => e.preventDefault());
 
   const addMenuName = () => {
@@ -56,10 +71,6 @@ function App() {
     addMenuName();
   });
 }
-
-// TODO 메뉴 수정
-// - [ ] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 prompt 창이 뜬다.
-// - [ ] 모달창에서 신규 메뉴명을 입력 받고, 확인 버튼을 누르면 메뉴가 수정된다.
 
 // TODO 메뉴 삭제
 // - [ ] 메뉴 삭제 버튼을 클릭하면 confirm 창이 뜬다.
